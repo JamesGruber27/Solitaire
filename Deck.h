5bfdd6f7d6f9bebdab5e91de0cb1815ae2698c1a
Jimboslice27 <jamesegruber@ufl.edu>
2016-03-29 18:20:43 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,78 +1,76 @@
+#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 
 class Node {
 private:
-  int number;
-  string suit;
-  Node *next;
+	int number;
+	string suit;
+	Node *next;
 
 public:
-  void SetNum(int n) {number = n;}
-  void SetNext(Node *a) {next = a;}
-  void SetSuit(string n) {suit = n;}
-  int Num() {return number;}
-  Node *Next() {return next;}
-  string Suit() {return suit;}
+	void SetNum(int n) { number = n; }
+	void SetNext(Node *a) { next = a; }
+	void SetSuit(char n) { suit=n; }
+	int Num() { return number; }
+	Node *Next() { return next; }
+	string Suit() { return suit; }
 };
 
 class List {
 private:
-  Node *head;
-  
+	Node *head;
+
 public:
-  List();
-  void Print();
+	List(vector<string> cards);
+	void Print();
 };
 
-List::List() {
-  head = new Node();
-  Node *temp = head;
-  for(int i = 0; i < 4; i++){
-    for(int j = 1; j < 14; j++){
-      temp->SetNum(j);
-      if(i==0){
-        temp->SetSuit("C");
-      }
-      else if (i==1){
-        temp->SetSuit("D");
-      }
-      else if (i==2){
-        temp->SetSuit("S");
-      }
-      else if (i==3){
-        temp->SetSuit("H");
-      }
-      if(i==3 && j==13){
-        break;
-      }
-      Node *card = new Node();
-      temp->SetNext(card);
-      temp=temp->Next();
-    }
-  }
+List::List(vector<string> cards) {
+	head = new Node();
+	Node *temp = head;
+	for (int i = 0; i < 52; i++) {
+		string suit = cards[i];
+		if (suit[1] == 48 || suit[1] == 49 || suit[1] == 50 || suit[1] == 51) {
+			temp->SetSuit(suit[2]);
+			int num1 = suit[0];
+			int num2 = suit[1];
+			int num = num1 + num2;
+			num = num - 97;
+			temp->SetNum(num + 10);
+		}
+		else {
+			temp->SetSuit(suit[1]);
+			int num = suit[0];
+			num = num - 48;
+			temp->SetNum(num);
+		}
+		Node *card = new Node();
+		temp->SetNext(card);
+		temp = temp->Next();
+	}
 }
 
-void List::Print(){
-  Node *temp = head;
-  for(int i = 0; i < 52; i++){
-    if(temp->Num() == 1){
-      cout<<"A";
-    }
-    else if(temp->Num() == 11){
-      cout<<"J";
-    }
-    else if(temp->Num()==12){
-      cout<<"Q";
-    }
-    else if(temp->Num()==13){
-      cout<<"K";
-    }
-    else cout<<temp->Num();
-    cout<<temp->Suit()<<" -> ";
-    temp = temp->Next();
-  }
-  cout<<"NULL"<<endl;
-  return;
+void List::Print() {
+	Node *temp = head;
+	for (int i = 0; i < 52; i++) {
+		if (temp->Num() == 1) {
+			cout << "A";
+		}
+		else if (temp->Num() == 11) {
+			cout << "J";
+		}
+		else if (temp->Num() == 12) {
+			cout << "Q";
+		}
+		else if (temp->Num() == 13) {
+			cout << "K";
+		}
+		else cout << temp->Num();
+		cout << temp->Suit() << "\t";
+		temp = temp->Next();
+	}
+	cout << endl;
+	return;
 }
