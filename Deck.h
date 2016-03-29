f4285d152c100ee2d639e46ac07f7809872b4944
Jimboslice27 <jamesegruber@ufl.edu>
2016-03-29 17:10:44 -0400

    Create Deck.h
    This header file creates the entire deck as a linked list. The current functions just create the deck and print it to the console.

---
+++ Deck.h
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+private:
+  int number;
+  string suit;
+  Node *next;
+
+public:
+  void SetNum(int n) {number = n;}
+  void SetNext(Node *a) {next = a;}
+  void SetSuit(string n) {suit n;}
+  int Num() {return number;}
+  Node *Next() {return next;}
+  string Suit() {return suit;}
+};
+
+class List {
+private:
+  Node *head;
+  
+public:
+  List();
+  void Print();
+};
+
+List::List() {
+  head = new Node();
+  Node *temp = head;
+  for(int i = 0; i < 4; i++){
+    for(int j = 1; j < 14; j++){
+      temp->SetNum(j);
+      if(i==0){
+        temp->SetSuit("C");
+      }
+      else if (i==1){
+        temp->SuitSuit("D");
+      }
+      else if (i==2){
+        temp->SetSuit("S");
+      }
+      else if (i==3){
+        temp->SetSuit("H");
+      }
+      if(i==3 && j==13){
+        break;
+      }
+      Node *card = new Node();
+      temp->SetNext(card);
+      temp=temp->Next();
+    }
+  }
+}
+
+void List::Print(){
+  Node *temp = head;
+  for(int i = 0; i < 52; i++){
+    if(temp->Num() == 1){
+      cout<<"A";
+    }
+    else if(temp->Num() == 11){
+      cout<<"J";
+    }
+    else if(temp->Num()==12){
+      cout<<"Q";
+    }
+    else if(temp->Num()==13){
+      cout<<"K";
+    }
+    else cout<<temp->Num();
+    cout<<temp->Suit()<<" -> ";
+    temp = temp->Next();
+  }
+  cout<<"NULL"<<endl;
+  return;
+}
