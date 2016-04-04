85de94f00b2840e42a9ac832df7e3a3b077468c0
Jimboslice27 <jamesegruber@ufl.edu>
2016-04-03 23:01:42 -0400

    Update Solitaire.cpp

--- Solitaire.cpp
+++ Solitaire.cpp
@@ -1,80 +1,102 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <chrono>
-#include <random>
-#include <vector>
-#include "Deck.h"
-#include "Draw.h"
 using namespace std;
 
-int main() {
-	vector<string> shuffler;
-	shuffler.push_back("1C");
-	shuffler.push_back("2C");
-	shuffler.push_back("3C");
-	shuffler.push_back("4C");
-	shuffler.push_back("5C");
-	shuffler.push_back("6C");
-	shuffler.push_back("7C");
-	shuffler.push_back("8C");
-	shuffler.push_back("9C");
-	shuffler.push_back("10C");
-	shuffler.push_back("11C");
-	shuffler.push_back("12C");
-	shuffler.push_back("13C");
-	shuffler.push_back("1D");
-	shuffler.push_back("2D");
-	shuffler.push_back("3D");
-	shuffler.push_back("4D");
-	shuffler.push_back("5D");
-	shuffler.push_back("6D");
-	shuffler.push_back("7D");
-	shuffler.push_back("8D");
-	shuffler.push_back("9D");
-	shuffler.push_back("10D");
-	shuffler.push_back("11D");
-	shuffler.push_back("12D");
-	shuffler.push_back("13D");
-	shuffler.push_back("1S");
-	shuffler.push_back("2S");
-	shuffler.push_back("3S");
-	shuffler.push_back("4S");
-	shuffler.push_back("5S");
-	shuffler.push_back("6S");
-	shuffler.push_back("7S");
-	shuffler.push_back("8S");
-	shuffler.push_back("9S");
-	shuffler.push_back("10S");
-	shuffler.push_back("11S");
-	shuffler.push_back("12S");
-	shuffler.push_back("13S");
-	shuffler.push_back("1H");
-	shuffler.push_back("2H");
-	shuffler.push_back("3H");
-	shuffler.push_back("4H");
-	shuffler.push_back("5H");
-	shuffler.push_back("6H");
-	shuffler.push_back("7H");
-	shuffler.push_back("8H");
-	shuffler.push_back("9H");
-	shuffler.push_back("10H");
-	shuffler.push_back("11H");
-	shuffler.push_back("12H");
-	shuffler.push_back("13C");
-	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-	shuffle(shuffler.begin(), shuffler.end(), default_random_engine(seed));
-	List deck(shuffler);
+class Node {
+private:
+	int number;
+	string suit;
+	Node *next;
 
-	cout << "Here is your Deck: " << endl;
-	deck.Print();
-	cout << endl << "How many Cards would you like to draw? ";
-	int n;
-	cin >> n;
-	cout << endl;
+public:
+	void SetNum(int n) { number = n; }
+	void SetNext(Node *a) { next = a; }
+	void SetSuit(char n) { suit=n; }
+	int Num() { return number; }
+	Node *Next() { return next; }
+	string Suit() { return suit; }
+};
 
-	for (int i = 0; i < n; i++) {
-		deck = Draw(deck);
+class List {
+private:
+	Node *head;
+	int DeckLocation = 0;
+
+public:
+	List(vector<string> cards);
+	void Print();
+	Node *Draw();
+	int Loc();
+};
+
+List::List(vector<string> cards) {
+	head = new Node();
+	Node *temp = head;
+	for (int i = 0; i < 52; i++) {
+		string suit = cards[i];
+		if (suit[1] == 48 || suit[1] == 49 || suit[1] == 50 || suit[1] == 51) {
+			temp->SetSuit(suit[2]);
+			int num1 = suit[0];
+			int num2 = suit[1];
+			int num = num1 + num2;
+			num = num - 97;
+			temp->SetNum(num + 10);
+		}
+		else {
+			temp->SetSuit(suit[1]);
+			int num = suit[0];
+			num = num - 48;
+			temp->SetNum(num);
+		}
+		Node *card = new Node();
+		temp->SetNext(card);
+		temp = temp->Next();
 	}
-	return 0;
+}
+
+void List::Print() {
+	//Print out the deck. This is more of just a check.
+	Node *temp = head;
+	for (int i = 0; i < 52; i++) {
+		if (temp->Num() == 1) {
+			cout << "A";
+		}
+		else if (temp->Num() == 11) {
+			cout << "J";
+		}
+		else if (temp->Num() == 12) {
+			cout << "Q";
+		}
+		else if (temp->Num() == 13) {
+			cout << "K";
+		}
+		else cout << temp->Num();
+		cout << temp->Suit() << "\t";
+		temp = temp->Next();
+	}
+	cout << endl;
+	return;
+}
+
+Node *List::Draw() {
+	Node *temp = head;
+	//find where we are in the deck
+	for (int i = 0; i < DeckLocation; i++) {
+		if (temp->Next() == NULL) {
+			DeckLocation = 0;
+			cout << DeckLocation << "Yay" << endl;
+			return temp;
+		}
+		temp = temp->Next();
+	}
+	if (temp->Next() == NULL) {
+		DeckLocation = 0;
+		return temp;
+	}
+	DeckLocation++;
+	return temp;
+}
+
+int List::Loc() {
+	return DeckLocation;
 }
