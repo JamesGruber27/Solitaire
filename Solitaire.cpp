949c1d2952aaa032533ccd5424e1b5aa09f47259
Jimboslice27 <jamesegruber@ufl.edu>
2016-04-15 18:12:53 -0400

    ???????

--- Solitaire.cpp
+++ Solitaire.cpp
@@ -1,3 +1,4 @@
+
 #include "Deck.h"
 using namespace std;
 
@@ -55,57 +56,62 @@ int main() {
 	shuffler.push_back("11H");
 	shuffler.push_back("12H");
 	shuffler.push_back("13C");
-    
-    //Shuffle the 52-long vector
+
+	//Shuffle the 52-long vector
 	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 	shuffle(shuffler.begin(), shuffler.end(), default_random_engine(seed));
-    
-    //Creating the 52-card deck object
+
+	//Creating the 52-card deck object
 	List* deck = new List(shuffler);
-    
-    //Greetings
-    cout << "Welcome to Solitaire! Here is your starting board." << endl;
-    
-    //Printing the contents of the deck
+
+	//Greetings
+	cout << "Welcome to Solitaire! Here is your starting board." << endl;
+
+	//Printing the contents of the deck
 	deck->Print();
-    
-    //Create columns of the tableau
-    vector<Node*> col1 = deck->createColumn(1);
-    vector<Node*> col2 = deck->createColumn(2);
-    vector<Node*> col3 = deck->createColumn(3);
-    vector<Node*> col4 = deck->createColumn(4);
-    vector<Node*> col5 = deck->createColumn(5);
-    vector<Node*> col6 = deck->createColumn(6);
-    vector<Node*> col7 = deck->createColumn(7);
-    int initCount1 = 1, initCount2 = 2, initCount3 = 3, initCount4 = 4, initCount5 = 5, initCount6 = 6, initCount7 = 7;
-    
-    
-    //Print columns
-    deck->PrintCol(col1, col2, col3, col4, col5, col6, col7);
-    
-    //Print deck again to show you that the correct cards were taken out
-    deck->Print();
-    
-    deck->turnOver(col1);
-    deck->turnOver(col2);
-    deck->turnOver(col3);
-    deck->turnOver(col4);
-    deck->turnOver(col5);
-    deck->turnOver(col6);
-    deck->turnOver(col7);
+
+	//Create columns of the tableau
+	vector<Node*> col1 = deck->createColumn(1);
+	vector<Node*> col2 = deck->createColumn(2);
+	vector<Node*> col3 = deck->createColumn(3);
+	vector<Node*> col4 = deck->createColumn(4);
+	vector<Node*> col5 = deck->createColumn(5);
+	vector<Node*> col6 = deck->createColumn(6);
+	vector<Node*> col7 = deck->createColumn(7);
+	int initCount1 = 1, initCount2 = 2, initCount3 = 3, initCount4 = 4, initCount5 = 5, initCount6 = 6, initCount7 = 7;
+
+
+	//Print columns
+	deck->PrintCol(col1, col2, col3, col4, col5, col6, col7);
+
+	//Print deck again to show you that the correct cards were taken out
+	deck->Print();
+
+	deck->turnOver(col1);
+	deck->turnOver(col2);
+	deck->turnOver(col3);
+	deck->turnOver(col4);
+	deck->turnOver(col5);
+	deck->turnOver(col6);
+	deck->turnOver(col7);
 
 
 	cout << "Would you like a card? 0 is yes, 1 is no " << endl;
 	Node *temp;
-	int DDDRAW = 1;
-	cin >> DDDRAW;
-	while (DDDRAW = 0) {
-		temp = deck->Draw();
-		cout << temp->Num() << temp->Suit() << endl;
-		cin.clear();
-		cin.ignore();
-		cin >> DDDRAW;
+	int DRAW = 1;
+	cin >> DRAW;
+	while (true) {
+		if (DRAW == 1) {
+			break;
+		}
+		else if (DRAW == 0) {
+			temp = deck->Draw();
+			cout << temp->Num() << temp->Suit() << endl << endl;
+			cin.clear();
+			cin.ignore();
+			cin >> DRAW;
+		}
 	}
-    //NEED TO PUT A WHILE LOOP
-    return 0;
+	//NEED TO PUT A WHILE LOOP
+	return 0;
 }
